85f7b625be29abbf0f7488618a61fc627d887595
krashaleksandra <129373072+krashaleksandra@users.noreply.github.com>
2023-05-01 18:49:59 +0300

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -1,2 +1,23 @@
-# put my assignment here
+import rhinoscriptsyntax as rs
 import Rhino.Geometry as rg
+#Empty lists for coordinates of points
+listx = []
+listy = []
+listz = []
+
+#Finding max and min coordinates
+for i in point:
+    listx.append(i.X)
+    listy.append(i.Y)
+    listz.append(i.Z)
+maxx = max(listx)
+minx = min(listx)
+maxy = max(listy)
+miny = min(listy)
+maxz = max(listz)
+minz = min(listz)
+
+#Vertical 3D Bounding box 
+point1 = rg.Point3d(maxx,maxy,maxz)
+point2 = rg.Point3d(minx,miny,minz)
+a = rg.Box(rs.WorldXYPlane(), rg.Interval(maxx,minx), rg.Interval(maxy,miny), rg.Interval(maxz,minz))
